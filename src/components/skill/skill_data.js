949bf88238d83528f7412177c90bfc4037402d04
mhduusop <mhduusop@gmail.com>
2022-07-25 11:30:18 +0700

    changes year

--- src/components/skill/skill_data.js
+++ src/components/skill/skill_data.js
@@ -14,7 +14,7 @@ export const skillData = [
     {
         logo : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png",
         name : "Javascript",
-        experience : "1 year"
+        experience : "2 year"
     },
 
     {
